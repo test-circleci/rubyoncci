b46cca95d75dcb2866eab1f3ffa0135150568731
Huy Nguyen <huydongkhe@gmail.com>
2015-12-24 15:58:16 +0700

    huydk: change inject.sh to echo env variables

--- inject.sh
+++ inject.sh
@@ -8,3 +8,11 @@ ls -lsa
 echo $PATH
 
 echo 'no pull from develop, just commit and then send pr'
+
+echo $CIRCLE_ARTIFACTS
+echo $CIRCLE_BRANCH
+echo $CIRCLE_COMPARE_URL
+echo $CI_PULL_REQUESTS
+echo $CI_PULL_REQUEST
+echo $CIRCLE_ARTIFACTS
+echo $CIRCLE_USERNAME
