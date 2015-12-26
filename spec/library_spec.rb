136d899734334bb3b852bfc6ceae1f84fe002dfa
Yh Huy Dk <huydongkhe@yahoo.com>
2015-12-26 10:11:06 -0800

    yhuydk: build nay phai that bai (2 file) - test 6

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -54,7 +54,7 @@ describe "Library object" do
   #  books2 = lib2.books.map { |book| book.title }
   #  expect(books).to eql books2
   #end
-  #  conflict choi hahahhaaaaa  
+
   it "remove an existing books" do
     js_parttern = @lib.get_book( "JavaScript Patterns")
     expect(js_parttern).to be_a Book
