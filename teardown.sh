67d5b29275148e48ab147fa3705e11c996dad754
huydk <huydongkhe@gmail.com>
2015-12-27 15:23:52 +0700

    huydk: change eol

--- teardown.sh
+++ teardown.sh
@@ -5,15 +5,9 @@ set -x
 pwd
 ls -lsa
 
-#git checkout -b tempobc develop
-#git add .
-#git add -u
-#git commit -m 'tempobc: Save virtual machine before tear down'
-#git push origin tempobc
-
 cd ..
 
-#rm -rf rubyoncci
+rm -rf rubyoncci
 
 pwd
 ls -lsa
